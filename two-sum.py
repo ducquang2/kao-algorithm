b3b91caf600c6b621991dee9e94e359c2e28be3a
ducquang2 <quangbilun@gmail.com>
2020-11-29 09:57:51 +0700

    faster-way

--- two-sum.py
+++ two-sum.py
@@ -5,10 +5,16 @@ class Solution:
             remain = target - num
 
             # first-way
-            if remain not in finding_dict:
-                finding_dict[num] = i
-            else:
+            # if remain not in finding_dict:
+            #     finding_dict[num] = i
+            # else:
+            #     return [finding_dict[remain], i]
+
+            # second-way
+            if remain in finding_dict:
                 return [finding_dict[remain], i]
+            finding_dict[num] = i
+        return []
 
 
 Solution.twoSum(Solution, [1,5,4,4,3], 9)
