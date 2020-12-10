778ab9117635c62094966a84b513febb227c0c1c
ducquang2 <quangbilun@gmail.com>
2020-12-10 14:04:18 +0700

    half

---
+++ summary-ranges.py
@@ -0,0 +1,13 @@
+class Solution:
+    def summaryRanges(self, nums):
+        if len(nums) == 1:
+            return [str(nums[0])]
+        else:
+            temp = []
+            curr = 0
+            new = 1
+            while new < len(nums):
+                if nums[new - 1] + 1 == nums[new]:
+                    if new == len(nums) - 1:
+                        temp.append(f"{nums[l]}->{nums[r]}")
+
