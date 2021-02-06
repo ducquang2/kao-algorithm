8e26fb804b794d74de7433a1808e0d0cf7db359b
ducquang2 <quangbilun@gmail.com>
2021-02-06 20:02:41 +0700

    second method

--- 292-nim-game.py
+++ 292-nim-game.py
@@ -1,7 +1,8 @@
 class Solution:
     def canWinNim(self, n):
         # First way:
-        return '1' in bin(n)[-2:]
-
+        # return '1' in bin(n)[-2:]
+        # Second way:
+        return n%4 != 0
 
 Solution.canWinNim(Solution, 9)
